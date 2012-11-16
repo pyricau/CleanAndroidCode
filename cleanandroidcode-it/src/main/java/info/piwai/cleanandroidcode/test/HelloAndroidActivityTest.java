b30f1c6b26c48c6e94c93a1a36f9ecf2db270fce
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-16 11:09:13 +0100

    updating and importing into eclipse

--- cleanandroidcode-it/src/main/java/info/piwai/cleanandroidcode/test/HelloAndroidActivityTest.java
+++ cleanandroidcode-it/src/main/java/info/piwai/cleanandroidcode/test/HelloAndroidActivityTest.java
@@ -1,17 +1,16 @@
 package info.piwai.cleanandroidcode.test;
 
+import info.piwai.cleanandroidcode.HelloAndroidActivity;
 import android.test.ActivityInstrumentationTestCase2;
-import info.piwai.cleanandroidcode.*;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
-    public HelloAndroidActivityTest() {
-        super(HelloAndroidActivity.class); 
-    }
+	public HelloAndroidActivityTest() {
+		super(HelloAndroidActivity.class);
+	}
 
-    public void testActivity() {
-        HelloAndroidActivity activity = getActivity();
-        assertNotNull(activity);
-    }
+	public void testActivity() {
+		HelloAndroidActivity activity = getActivity();
+		assertNotNull(activity);
+	}
 }
-
