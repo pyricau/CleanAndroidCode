2ae95a3449d3c184310b7bd45ec986ab916ee042
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-17 17:50:24 +0100

    cleaning up, adding icons

--- cleanandroidcode/src/main/java/info/piwai/cleanandroidcode/HelloAndroidActivity.java
+++ cleanandroidcode/src/main/java/info/piwai/cleanandroidcode/HelloAndroidActivity.java
@@ -2,28 +2,15 @@ package info.piwai.cleanandroidcode;
 
 import info.piwai.cleanandroidcode.base.BaseActivity;
 
-import javax.inject.Inject;
-
-import android.os.Bundle;
-
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.squareup.otto.Subscribe;
 
 @EActivity(R.layout.hello_activity)
 public class HelloAndroidActivity extends BaseActivity {
 
-	@Inject
-	ActivityTitleUpdater titleUpdater;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		titleUpdater.onCreate(this);
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		titleUpdater.onDestroy();
+	@Subscribe
+	public void onUpdateTitle(UpdateTitleEvent event) {
+		setTitle(event.title);
 	}
 
 }
